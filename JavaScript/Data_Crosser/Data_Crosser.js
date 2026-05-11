04637458e6253e089b2152198402e7ae25c95895
Miillliiinn <thomasmilinjloba@gmail.com>
2026-05-11 03:00:06 +0200

    personal subject

--- JavaScript/Data_Crosser/Data_Crosser.js
+++ JavaScript/Data_Crosser/Data_Crosser.js
@@ -1,15 +1,3 @@
-const users = [
-  { id: 1, name: Thomas },
-  { id: 2, name: Julie },
-  { id: 3, name: Thomas}
-];
-const orders = [
-  { userId: 1, amount: 50 },
-  { userId: 1, amount: 20 },
-  { userId: 2, amount: 100 },
-  { userId: 3, amount: 90}
-];
-
 function collectDataAllAmount(users, orders)
 {
   const amounById = {};
